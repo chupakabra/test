f6ed316f1f4dfb8be75fce96b966cae040725395
chupakabra <daria.chernobaieva@gmail.com>
2013-07-10 16:53:33 +0300

    ticket:1. Modified according to comments

--- my_info/gen_info/views.py
+++ my_info/gen_info/views.py
@@ -1,6 +1,7 @@
 from models import Person
 from django.shortcuts import render, get_object_or_404
 
+
 def main(request):
     person = get_object_or_404(Person, pk=1)
     context = {'person': person}
