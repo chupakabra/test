e649a31e8f893e61f61b927b7306a7059cccd144
chupakabra <daria.chernobaieva@gmail.com>
2013-07-11 01:01:08 +0300

    ticket:3. Checked against PEP8

--- my_info/http_request_storage/models.py
+++ my_info/http_request_storage/models.py
@@ -1,8 +1,9 @@
 from django.db.models import *
 
+
 class HttpRequestStorage(Model):
-	date = DateTimeField()
-	path = CharField(max_length=100)
-	
-	def __unicode__(self):
-		return u'%s %s' % (self.date, self.path)
+    date = DateTimeField()
+    path = CharField(max_length=100)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.date, self.path)
--- my_info/http_request_storage/tests.py
+++ my_info/http_request_storage/tests.py
@@ -7,26 +7,30 @@ from django.core.urlresolvers import reverse
 
 
 class RequestStorageMiddlewareTests(TestCase):
-	
+
     def test_middleware_on(self):
-        """Check the case when RequestStorageMiddleware is specified in settings"""
+        """Check the case when RequestStorageMiddleware is specified
+        in settings"""
         response = self.client.get(reverse('http_requests:requests'))
         self.assertEqual(response.status_code, 200)
-        cur_request = HttpRequestStorage.objects.get(path=reverse('http_requests:requests'))
+        cur_request = HttpRequestStorage.objects.get(
+            path=reverse('http_requests:requests'))
         self.assertNotEqual(cur_request, None)
         response = self.client.get(reverse('http_requests:main'))
         self.assertEqual(response.status_code, 200)
-        self.assertNotEqual(HttpRequestStorage.objects.get(path=reverse('http_requests:main')), None)
-        
-        
-    @override_settings(MIDDLEWARE_CLASSES = (
-                       'django.middleware.common.CommonMiddleware',
-                       'django.contrib.sessions.middleware.SessionMiddleware',
-                       'django.middleware.csrf.CsrfViewMiddleware',
-                       'django.contrib.auth.middleware.AuthenticationMiddleware',
-                       'django.contrib.messages.middleware.MessageMiddleware'))
+        self.assertNotEqual(HttpRequestStorage.objects.get(
+                            path=reverse('http_requests:main')), None)
+
+    @override_settings(
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware'))
     def test_middleware_off(self):
-        """Check the case when RequestStorageMiddleware is not specified in settings"""
+        """Check the case when RequestStorageMiddleware is not specified
+        in settings"""
         response = self.client.get(reverse('http_requests:requests'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(HttpRequestStorage.objects.all(), [])
