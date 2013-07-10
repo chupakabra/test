edce176f643cdd42d2b79b8701cbdb9ad9399d0f
chupakabra <daria.chernobaieva@gmail.com>
2013-07-10 20:03:50 +0300

    ticket:1. Modified according to comments

--- my_info/gen_info/tests.py
+++ my_info/gen_info/tests.py
@@ -10,8 +10,8 @@ class PersonViewTests(TestCase):
         response = self.client.get(reverse('gen_info:main'))
         self.assertEqual(response.status_code, 404)
 
-    """Context checking"""
     def test_main(self):
+        """Context checking"""
         person = Person.objects.create(
             name="Name", last_name="Last_Name",
             birth_date="1929-03-03", bio="FDGDSHFG",
--- my_info/urls.py
+++ my_info/urls.py
@@ -5,14 +5,5 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('gen_info.urls', namespace="gen_info")),
-    # Examples:
-    # url(r'^$', 'my_info.views.home', name='home'),
-    # url(r'^my_info/', include('my_info.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^$', include('gen_info.urls',
+                           namespace="gen_info")))
