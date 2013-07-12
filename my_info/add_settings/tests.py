2768e7484a71528f9709d24ac26c53072e6a1000
chupakabra <daria.chernobaieva@gmail.com>
2013-07-12 03:01:10 +0300

    ticket:4.Checked against PEP8

--- my_info/add_settings/tests.py
+++ my_info/add_settings/tests.py
@@ -15,19 +15,18 @@ class ContextProcessorSettingsTest(TestCase):
         self.assertContains(response, 'STATICFILES_FINDERS')
 
     """@override_settings(
-	    TEMPLATE_CONTEXT_PROCESSORS=(
-	        'django.contrib.auth.context_processors.auth',
-	        'django.core.context_processors.debug',
-	        'django.core.context_processors.i18n',
-	        'django.core.context_processors.media',
-	        'django.core.context_processors.static',
-	        'django.core.context_processors.tz',
-	        'django.contrib.messages.context_processors.messages'))            
-    def test_template_context_processor_off(self):
-	    \"""Check the case when context_processor.settings is not specified
-	    in settings\"""
-	    response = self.client.get(reverse('add_settings:settings'))
-	    self.assertEqual(response.status_code, 200)
-	    self.assertEqual(response.context['settings'], {})
-	    self.assertNotContains(response, 'STATICFILES_FINDERS')"""
-
+           TEMPLATE_CONTEXT_PROCESSORS=(
+               'django.contrib.auth.context_processors.auth',
+               'django.core.context_processors.debug',
+               'django.core.context_processors.i18n',
+               'django.core.context_processors.media',
+               'django.core.context_processors.static',
+               'django.core.context_processors.tz',
+               'django.contrib.messages.context_processors.messages'))
+     def test_template_context_processor_off(self):
+        \"""Check the case when context_processor.settings is not specified
+        in settings\"""
+        response = self.client.get(reverse('add_settings:settings'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['settings'], {})
+        self.assertNotContains(response, 'STATICFILES_FINDERS')"""
--- my_info/add_settings/views.py
+++ my_info/add_settings/views.py
@@ -4,6 +4,6 @@ from context_processor import settings_processor
 
 
 def main(request):
-	return render(request, 'add_settings/django_settings.html', 
-	   context_instance=RequestContext(request, processors=[settings_processor]))
-
+    return render(request, 'add_settings/django_settings.html',
+                  context_instance=RequestContext(request,
+                  processors=[settings_processor]))
